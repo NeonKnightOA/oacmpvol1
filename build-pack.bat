f1d68025a8852b1736e36a3b4e5c90aa36bc2566
NeonKnightOA <openarena.arg@gmail.com>
2023-04-29 18:29:51 -0300

    Changes to dm2, dm5, dm6, dm8 and dm9 + buildscript
    - All five maps subjected to AutoCaulk and compiled without -dark nor -dirty.
    - dm2: Removed botclips and clusterportals. Should fix the problem with bots not being able to reach the MH.

--- build-pack.bat
+++ build-pack.bat
@@ -9,16 +9,17 @@ rem -----------------------------------------------------
 
 echo Compressing...
 cd baseoa
-..\zip.exe -9r ..\z_oacmp1_beta.pk3 levelshots\*.jpg
-..\zip.exe -9r ..\z_oacmp1_beta.pk3 levelshots\*.tga
+..\zip.exe -9r ..\z_oacmp1_beta.pk3 levelshots\*.*
 ..\zip.exe -9r ..\z_oacmp1_beta.pk3 maps\*.aas
 ..\zip.exe -9r ..\z_oacmp1_beta.pk3 maps\*.bsp
 ..\zip.exe -9r ..\z_oacmp1_beta.pk3 models\*.*
+..\zip.exe -9r ..\z_oacmp1_beta.pk3 music\*.*
 ..\zip.exe -9r ..\z_oacmp1_beta.pk3 scripts\*.arena
 ..\zip.exe -9r ..\z_oacmp1_beta.pk3 scripts\*.shader
 ..\zip.exe -9r ..\z_oacmp1_beta.pk3 sound\*.*
 ..\zip.exe -9r ..\z_oacmp1_beta.pk3 textures\*.*
 ..\zip.exe -9r ..\z_oacmp1_beta.pk3 *.cfg
+..\zip.exe -9r ..\z_oacmp1_beta.pk3 docs\COPYING
 echo Done
 
 @echo on
