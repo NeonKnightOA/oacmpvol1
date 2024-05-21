7cd8ce0273a73ec5c57c6fc1ed4311cd9fd777d8
Herman Yanush <gera@disroot.org>
2024-05-21 13:51:39 +0300

    use forward slashes in unix script

--- build-pack.sh
+++ build-pack.sh
@@ -7,17 +7,17 @@
 # -----------------------------------------------------
 echo Compressing...
 cd baseoa
-zip -9r ..\z_oacmp1_beta.pk3 levelshots\*.*
-zip -9r ..\z_oacmp1_beta.pk3 maps\*.aas
-zip -9r ..\z_oacmp1_beta.pk3 maps\*.bsp
-zip -9r ..\z_oacmp1_beta.pk3 models\*.*
-zip -9r ..\z_oacmp1_beta.pk3 music\*.*
-zip -9r ..\z_oacmp1_beta.pk3 scripts\*.arena
-zip -9r ..\z_oacmp1_beta.pk3 scripts\*.shader
-zip -9r ..\z_oacmp1_beta.pk3 sound\*.*
-zip -9r ..\z_oacmp1_beta.pk3 textures\*.*
-zip -9r ..\z_oacmp1_beta.pk3 *.cfg
-zip -9r ..\z_oacmp1_beta.pk3 docs\COPYING
+zip -9r ../z_oacmp1_beta.pk3 levelshots/*.*
+zip -9r ../z_oacmp1_beta.pk3 maps/*.aas
+zip -9r ../z_oacmp1_beta.pk3 maps/*.bsp
+zip -9r ../z_oacmp1_beta.pk3 models/*.*
+zip -9r ../z_oacmp1_beta.pk3 music/*.*
+zip -9r ../z_oacmp1_beta.pk3 scripts/*.arena
+zip -9r ../z_oacmp1_beta.pk3 scripts/*.shader
+zip -9r ../z_oacmp1_beta.pk3 sound/*.*
+zip -9r ../z_oacmp1_beta.pk3 textures/*.*
+zip -9r ../z_oacmp1_beta.pk3 *.cfg
+zip -9r ../z_oacmp1_beta.pk3 docs/COPYING
 echo Done
 
 exit 0
